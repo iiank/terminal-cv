7aded05661be1362319965fbdaea5a2938f27f5e
iiank <iiankhr@gmail.com>
2026-09-12 23:58:26 +0800

    Add output cell to display github link and preview of respective project

--- src/components/pages/Projects.vue
+++ src/components/pages/Projects.vue
@@ -3,6 +3,12 @@ import data from '../../data/projects.json';
 import { toLines } from '../../utils/toLines.js';
 
 const projects = data.projects;
+
+/* GitHub's OpenGraph endpoint returns the same card shown when a repository
+   link is shared, so the cell output looks like a rendered embed. */
+function previewUrl(repo) {
+  return 'https://opengraph.githubassets.com/1/' + repo;
+}
 </script>
 
 <template>
@@ -12,12 +18,40 @@ const projects = data.projects;
       <span class="notebook__kernel">{{ data.kernel }}</span>
     </div>
 
-    <div v-for="(project, index) in projects" :key="project.title" class="cell">
-      <div class="cell__marker">[{{ index + 1 }}]</div>
-      <div class="cell__box">
-        <p class="cell__title">{{ project.title }}</p>
-        <p v-for="(paragraph, part) in toLines(project.body)" :key="part" class="cell__body">{{ paragraph }}</p>
+    <template v-for="(project, index) in projects" :key="project.title">
+      <div class="cell">
+        <div class="cell__marker">[{{ index + 1 }}]</div>
+        <div class="cell__box">
+          <p class="cell__title">{{ project.title }}</p>
+          <p v-for="(paragraph, part) in toLines(project.body)" :key="part" class="cell__body">{{ paragraph }}</p>
+        </div>
       </div>
-    </div>
+
+      <div v-if="project.repo" class="cell cell--output">
+        <div class="cell__marker cell__marker--out">Out[{{ index + 1 }}]</div>
+        <div class="cell__result">
+          <a
+            class="cell__repo"
+            :href="'https://github.com/' + project.repo"
+            target="_blank"
+            rel="noopener noreferrer"
+            v-text="'github.com/' + project.repo"
+          ></a>
+
+          <a
+            class="cell__preview"
+            :href="'https://github.com/' + project.repo"
+            target="_blank"
+            rel="noopener noreferrer"
+            tabindex="-1"
+            aria-hidden="true"
+          ><img
+            :src="previewUrl(project.repo)"
+            :alt="'Repository card for ' + project.repo"
+            loading="lazy"
+          ></a>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
